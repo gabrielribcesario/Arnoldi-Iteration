a0bf606205f2fa0530bf3d6cfcbc7c278db05a16
gabrielribcesario <gabrielc.ribeiro@hotmail.com>
2025-07-14 00:35:40 -0300

    Add prototype of Francis' Algorithm to src and remove quad precision

--- src/ritz.f95
+++ src/ritz.f95
@@ -1,51 +1,72 @@
 program ritz
     use arnoldi
+    use francis
     implicit none
 
-    ! gfortran ritz.f95 arnoldi.f95 -o ritz -O3
+    ! gfortran arnoldi.f95 francis.f95 ritz.f95 -o ritz -O2
 
+    ! Format specifiers: https://pages.mtu.edu/~shene/courses/cs201/notes/chap05/format.html
     character(20), parameter :: format_specifier = '(*(ES20.12, :, ","))'
-    integer, parameter :: wp = 8 ! Working precision (bytes)
-    integer, parameter :: m = 5000, n = 600 ! rank(a), dim(k^n)
+    integer, parameter :: m = 200 ! rank(A)
+    integer, parameter :: n = 200 ! Dimension of the Krylov subspace
     integer, allocatable :: seed(:)
     integer :: i, seed_size = 12
-    integer :: file1 = 20, file2 = 21, file3 = 22, file4 = 23
-    real(wp) :: A(m, m), b(m) ! Input matrix and vector
-    real(wp) :: Q(m, n), H(n, n - 1) ! Orthonormal basis and hessenberg matrix
+    integer :: file1 = 20, file2 = 21, file3 = 22, file4 = 23, file5 = 24
+    real(8) :: A(m,m) ! Input matrix
+    real(8) :: b(m) ! Input vector
+    real(8) :: Q(m,n) ! Orthonormal basis
+    real(8) :: H_hat(n,n-1) ! Hessenberg reduction output
+    real(8), allocatable :: H(:,:) ! Upper hessenberg matrix
+    integer :: erank ! # of Arnoldi iterations ran (i.e. dim of eigenspace)
 
-    open(file1, file="../data/A.dat", status="replace", action="write")
-    open(file2, file="../data/b.dat", status="replace", action="write")
-    open(file3, file="../data/Q.dat", status="replace", action="write")
-    open(file4, file="../data/H.dat", status="replace", action="write")
-
-    ! random seed initialization
+    ! Random seed initialization
     call random_seed(size=seed_size)
     allocate(seed(seed_size))
     seed = 42
     call random_seed(put=seed)
     deallocate(seed)
 
+
     ! Initialize matrix and vector to random values
+    open(file1, file="../data/A.dat", status="replace", action="write")
+    open(file2, file="../data/b.dat", status="replace", action="write")
     call random_number(b(:)) ! range [0.0, 1.0]
     do i = 1, m
-        call random_number(A(i, :))
-        A(i, :) = A(i, :) - 0.5_wp ! range [-0.5, 0.5]
-        write(file1, format_specifier) A(i, :) ! Format specifiers: https://pages.mtu.edu/~shene/courses/cs201/notes/chap05/format.html
+        call random_number(A(i,:))
+        A(i,:) = A(i,:) - 0.5_8 ! range [-0.5, 0.5]
+        write(file1, format_specifier) A(i,:) 
         write(file2, format_specifier) b(i)
     end do
-
-    call arnoldi_iteration(m, n, A, b, Q, H)
-
-    do i = 1, m
-        write(file3, format_specifier) Q(i, :)
-    end do
-
-    do i = 1, n
-        write(file4, format_specifier) H(i, :)
-    end do
-
     close(file1)
     close(file2)
+
+    ! Hessenberg matrix decomposition
+    call arnoldi_iteration(m, n, A, b, Q, H_hat, erank)
+
+    open(file3, file="../data/Q.dat", status="replace", action="write")
+    do i = 1, m
+        write(file3, format_specifier) Q(i,:erank)
+    end do
     close(file3)
+
+    open(file4, file="../data/H.dat", status="replace", action="write")
+    do i = 1, erank
+        write(file4, format_specifier) H_hat(i,:erank)
+    end do
     close(file4)
+
+    allocate(H(erank,erank))
+
+    H = H_hat(:erank,:erank)
+    call francis_algorithm(m, H)
+
+    open(file5, file="../data/A_hat.dat", status="replace", action="write")
+    do i = 1, erank
+        write(file5, format_specifier) H(i,:)
+    end do
+    close(file5)
+    
+    deallocate(H)
+
+    print *, "Done."
 end program
